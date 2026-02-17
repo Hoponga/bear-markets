072335e3868199f8ef27cd2be46820eaa6bc5011
ARtheboss <30683624+ARtheboss@users.noreply.github.com>
2026-02-16 22:44:45 -0800

    More UI updates

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -49,7 +49,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="bg-transparent sticky top-0 z-40">
+      <nav className="bg-[rgb(17,24,39)] sticky top-0 z-40">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-12">
             {/* Left - Logo */}
